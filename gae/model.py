897417afdfede22bfcc4f9e731b59ec25ee38fb8
paulmorio <pms69@cam.ac.uk>
2019-06-11 12:24:18 +0100

    Fixed training code which failed with type error

--- gae/model.py
+++ gae/model.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from gae.layers import GraphConvolution
+# from gae.layers import GraphConvolution
 
+from layers import GraphConvolution
 
 class GCNModelVAE(nn.Module):
     def __init__(self, input_feat_dim, hidden_dim1, hidden_dim2, dropout):
@@ -43,3 +44,29 @@ class InnerProductDecoder(nn.Module):
         z = F.dropout(z, self.dropout, training=self.training)
         adj = self.act(torch.mm(z, z.t()))
         return adj
+
+
+class SGCModelVAE(nn.Module):
+    def __init__(self, input_feat_dim, hidden_dim1, hidden_dim2, dropout):
+        super(SGCModelVAE, self).__init__()
+        self.gc1 = GraphConvolution(input_feat_dim, hidden_dim1, dropout, act=F.relu)
+        self.gc2 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
+        self.gc3 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
+        self.dc = InnerProductDecoder(dropout, act=lambda x: x)
+
+    def encode(self, x, adj):
+        hidden1 = self.gc1(x, adj)
+        return self.gc2(hidden1, adj), self.gc3(hidden1, adj)
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = torch.exp(logvar)
+            eps = torch.randn_like(std)
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def forward(self, x, adj):
+        mu, logvar = self.encode(x, adj)
+        z = self.reparameterize(mu, logvar)
+        return self.dc(z), mu, logvar
--- gae/train.py
+++ gae/train.py
@@ -9,9 +9,13 @@ import scipy.sparse as sp
 import torch
 from torch import optim
 
-from gae.model import GCNModelVAE
-from gae.optimizer import loss_function
-from gae.utils import load_data, mask_test_edges, preprocess_graph, get_roc_score
+# from gae.model import GCNModelVAE
+# from gae.optimizer import loss_function
+# from gae.utils import load_data, mask_test_edges, preprocess_graph, get_roc_score
+
+from model import GCNModelVAE
+from optimizer import loss_function
+from utils import load_data, mask_test_edges, preprocess_graph, get_roc_score
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='gcn_vae', help="models used")
@@ -26,58 +30,59 @@ parser.add_argument('--dataset-str', type=str, default='cora', help='type of dat
 args = parser.parse_args()
 
 
-def gae_for(args):
-    print("Using {} dataset".format(args.dataset_str))
-    adj, features = load_data(args.dataset_str)
-    n_nodes, feat_dim = features.shape
+# def gae_for(args):
+print("Using {} dataset".format(args.dataset_str))
+adj, features = load_data(args.dataset_str)
+n_nodes, feat_dim = features.shape
 
-    # Store original adjacency matrix (without diagonal entries) for later
-    adj_orig = adj
-    adj_orig = adj_orig - sp.dia_matrix((adj_orig.diagonal()[np.newaxis, :], [0]), shape=adj_orig.shape)
-    adj_orig.eliminate_zeros()
+# Store original adjacency matrix (without diagonal entries) for later
+adj_orig = adj
+adj_orig = adj_orig - sp.dia_matrix((adj_orig.diagonal()[np.newaxis, :], [0]), shape=adj_orig.shape)
+adj_orig.eliminate_zeros()
 
-    adj_train, train_edges, val_edges, val_edges_false, test_edges, test_edges_false = mask_test_edges(adj)
-    adj = adj_train
+adj_train, train_edges, val_edges, val_edges_false, test_edges, test_edges_false = mask_test_edges(adj)
+adj = adj_train
 
-    # Some preprocessing
-    adj_norm = preprocess_graph(adj)
-    adj_label = adj_train + sp.eye(adj_train.shape[0])
-    # adj_label = sparse_to_tuple(adj_label)
-    adj_label = torch.FloatTensor(adj_label.toarray())
+# Some preprocessing
 
-    pos_weight = float(adj.shape[0] * adj.shape[0] - adj.sum()) / adj.sum()
-    norm = adj.shape[0] * adj.shape[0] / float((adj.shape[0] * adj.shape[0] - adj.sum()) * 2)
+adj_norm = preprocess_graph(adj)
+adj_label = adj_train + sp.eye(adj_train.shape[0])
+# adj_label = sparse_to_tuple(adj_label)
+adj_label = torch.FloatTensor(adj_label.toarray())
 
-    model = GCNModelVAE(feat_dim, args.hidden1, args.hidden2, args.dropout)
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+pos_weight = float(adj.shape[0] * adj.shape[0] - adj.sum()) / adj.sum()
+norm = adj.shape[0] * adj.shape[0] / float((adj.shape[0] * adj.shape[0] - adj.sum()) * 2)
 
-    hidden_emb = None
-    for epoch in range(args.epochs):
-        t = time.time()
-        model.train()
-        optimizer.zero_grad()
-        recovered, mu, logvar = model(features, adj_norm)
-        loss = loss_function(preds=recovered, labels=adj_label,
-                             mu=mu, logvar=logvar, n_nodes=n_nodes,
-                             norm=norm, pos_weight=pos_weight)
-        loss.backward()
-        cur_loss = loss.item()
-        optimizer.step()
+model = GCNModelVAE(feat_dim, args.hidden1, args.hidden2, args.dropout)
+optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
-        hidden_emb = mu.data.numpy()
-        roc_curr, ap_curr = get_roc_score(hidden_emb, adj_orig, val_edges, val_edges_false)
+hidden_emb = None
+for epoch in range(args.epochs):
+    t = time.time()
+    model.train()
+    optimizer.zero_grad()
+    recovered, mu, logvar = model(features, adj_norm)
+    loss = loss_function(preds=recovered, labels=adj_label,
+                         mu=mu, logvar=logvar, n_nodes=n_nodes,
+                         norm=norm, pos_weight=torch.tensor(pos_weight))
+    loss.backward()
+    cur_loss = loss.item()
+    optimizer.step()
 
-        print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(cur_loss),
-              "val_ap=", "{:.5f}".format(ap_curr),
-              "time=", "{:.5f}".format(time.time() - t)
-              )
+    hidden_emb = mu.data.numpy()
+    roc_curr, ap_curr = get_roc_score(hidden_emb, adj_orig, val_edges, val_edges_false)
 
-    print("Optimization Finished!")
+    print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(cur_loss),
+          "val_ap=", "{:.5f}".format(ap_curr),
+          "time=", "{:.5f}".format(time.time() - t)
+          )
 
-    roc_score, ap_score = get_roc_score(hidden_emb, adj_orig, test_edges, test_edges_false)
-    print('Test ROC score: ' + str(roc_score))
-    print('Test AP score: ' + str(ap_score))
+print("Optimization Finished!")
+
+roc_score, ap_score = get_roc_score(hidden_emb, adj_orig, test_edges, test_edges_false)
+print('Test ROC score: ' + str(roc_score))
+print('Test AP score: ' + str(ap_score))
 
 
-if __name__ == '__main__':
-    gae_for(args)
+# if __name__ == '__main__':
+#     gae_for(args)
